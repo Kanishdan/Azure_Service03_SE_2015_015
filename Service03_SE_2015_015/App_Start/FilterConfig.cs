5804ba13c25fcb0e5a66273173746f9791e25743
Yogarasa kanishdan <31471087+Kanishdan@users.noreply.github.com>
2020-08-12 20:05:01 +0530

    Add project files.

---
+++ Service03_SE_2015_015/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Service03_SE_2015_015
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Service03_SE_2015_015/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Service03_SE_2015_015.MvcApplication" Language="C#" %>
